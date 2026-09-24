bf3b9e57883d76b884cabd5c45e27ff9d0f0812e
Jianping145 <jianpingwu145@gmail.com>
2026-09-24 16:06:22 +0800

    Add files via upload

--- 色播聚合.py
+++ 色播聚合.py
@@ -20,7 +20,8 @@ class Spider(Spider):
 			{
 				"vod_id": "/" + item['address'],
 				"vod_name": item['title'],
-				"vod_pic": item['xinimg'],
+				"vod_pic": item['xinimg'].replace("http://cdn.gcufbd.top/img/",
+												  "https://slink.ltd/https://raw.githubusercontent.com/fish2018/lib/refs/heads/main/imgs/"),
 				"vod_remarks": item['Number'],
 				"style": {"type": "rect", "ratio": 1.33}
 			} for item in sorted(data, key=lambda x: int(x['Number']), reverse=True)
@@ -42,7 +43,7 @@ class Spider(Spider):
 		vod = [{
 			"vod_play_from": 'sebo',
 			"vod_play_url": playUrls,
-			"vod_content": '简介',
+			"vod_content": 'https://github.com/fish2018',
 		}]
 		result = {"list": vod}
 		return result
